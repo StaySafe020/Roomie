eb318bdaa60cfef800ac7274fa00aba47e1a805a
StaySafe020 <jambongralpher@gmail.com>
2025-03-07 11:47:15 +0100

    deleted main files

--- roomie_rental_platform/Roomieapp/exceptions.py
+++
@@ -1,7 +0,0 @@
-
-from rest_framework.exceptions import APIException
-
-class CustomAPIException(APIException):
-    status_code = 400
-    default_detail = 'A server error occured'
-    default_code = 'error'
